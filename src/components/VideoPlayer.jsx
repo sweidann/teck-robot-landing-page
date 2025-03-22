b645e9101e912d19dfd0ac87a432800c24ea9ba2
sweidann <m.sweidan74@gmail.com>
2025-03-22 03:20:58 +0200

    3rd try

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -7,8 +7,22 @@ const VideoPlayer = ({ url, style, playing }) => {
         url={url}
         playing={playing}
         muted
+        playsinline
         width="100%"
         height="100%"
+        config={{
+          file: {
+            attributes: {
+              playsInline: true,
+              webkitPlaysinline: "true",
+              preload: "auto",
+              autoPlay: true,
+              muted: true,
+            },
+            forceVideo: true,
+            forceHLS: true,
+          },
+        }}
       />
     </div>
   );
